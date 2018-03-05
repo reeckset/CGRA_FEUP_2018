0b60cdd175694b2c330e5f790edb6fe9290d2983
reeckset <henriquemlima7@gmail.com>
2018-03-05 16:52:28 +0000

    [TP2] Done exercise 2

--- TP2/tp2.2.8/MyQuad.js
+++ TP2/tp2.2.8/MyQuad.js
@@ -14,6 +14,7 @@ class MyQuad extends CGFobject
 
 	initBuffers()
 	{
+
 		this.vertices = [
 				-0.5, -0.5, 0,
 				0.5, -0.5, 0,
@@ -27,6 +28,14 @@ class MyQuad extends CGFobject
 			];
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		]
+
 		this.initGLBuffers();
 
 	};
