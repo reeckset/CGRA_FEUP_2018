83542c6610964e5b4944ef7c8c9f181e4db833a0
reeckset <henriquemlima7@gmail.com>
2018-03-05 16:28:28 +0000

    Structured folder

--- TP2/tp2.2/LightingScene.js
+++ TP2/tp2.2/LightingScene.js
@@ -6,14 +6,14 @@ var BOARD_HEIGHT = 4.0;
 var BOARD_A_DIVISIONS = 1;
 var BOARD_B_DIVISIONS = 100;
 
-class LightingScene extends CGFscene 
+class LightingScene extends CGFscene
 {
 	constructor()
 	{
 		super();
 	};
 
-	init(application) 
+	init(application)
 	{
 		super.init(application);
 
@@ -33,13 +33,13 @@ class LightingScene extends CGFscene
 		this.table = new MyTable(this);
 		this.wall = new Plane(this);
 		this.floor = new MyQuad(this);
-		
+
 		this.boardA = new Plane(this, BOARD_A_DIVISIONS);
 		this.boardB = new Plane(this, BOARD_B_DIVISIONS);
 
 		// Materials
 		this.materialDefault = new CGFappearance(this);
-		
+
 		this.materialA = new CGFappearance(this);
 		this.materialA.setAmbient(0.3,0.3,0.3,1);
 		this.materialA.setDiffuse(0.6,0.6,0.6,1);
@@ -49,24 +49,24 @@ class LightingScene extends CGFscene
 		this.materialB = new CGFappearance(this);
 		this.materialB.setAmbient(0.3,0.3,0.3,1);
 		this.materialB.setDiffuse(0.6,0.6,0.6,1);
-		this.materialB.setSpecular(0.8,0.8,0.8,1);	
+		this.materialB.setSpecular(0.8,0.8,0.8,1);
 		this.materialB.setShininess(120);
-		
+
 	};
 
-	initCameras() 
+	initCameras()
 	{
 		this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30, 30, 30), vec3.fromValues(0, 0, 0));
 	};
 
-	initLights() 
+	initLights()
 	{
 		this.setGlobalAmbientLight(0.5,0.5,0.5, 1.0);
-		
+
 		// Positions for four lights
 		this.lights[0].setPosition(4, 6, 1, 1);
 		this.lights[0].setVisible(true); // show marker on light position (different from enabled)
-		
+
 		this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
 		this.lights[1].setVisible(true); // show marker on light position (different from enabled)
 
@@ -77,21 +77,21 @@ class LightingScene extends CGFscene
 
 		this.lights[0].setAmbient(0, 0, 0, 1);
 		this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-		//this.lights[0].enable();
+		this.lights[0].enable();
 
 		this.lights[1].setAmbient(0, 0, 0, 1);
 		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
 		//this.lights[1].enable();
 	};
 
-	updateLights() 
+	updateLights()
 	{
 		for (var i = 0; i < this.lights.length; i++)
 			this.lights[i].update();
 	}
 
 
-	display() 
+	display()
 	{
 		// ---- BEGIN Background, camera and axis setup
 
@@ -157,7 +157,7 @@ class LightingScene extends CGFscene
 		this.pushMatrix();
 			this.translate(4, 4.5, 0.2);
 			this.scale(BOARD_WIDTH, BOARD_HEIGHT, 1);
-			
+
 			this.materialA.apply();
 			this.boardA.display();
 		this.popMatrix();
@@ -166,7 +166,7 @@ class LightingScene extends CGFscene
 		this.pushMatrix();
 			this.translate(10.5, 4.5, 0.2);
 			this.scale(BOARD_WIDTH, BOARD_HEIGHT, 1);
-			
+
 			this.materialB.apply();
 			this.boardB.display();
 		this.popMatrix();
